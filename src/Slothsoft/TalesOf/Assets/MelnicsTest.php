2e04689744f3d4c5c50450316e3026e2db7dd47d
Faulo <FauloLio@gmail.com>
2018-04-28 11:35:46 +0200

    new Kernel API

--- src/Slothsoft/TalesOf/Assets/MelnicsTest.php
+++ src/Slothsoft/TalesOf/Assets/MelnicsTest.php
@@ -2,9 +2,9 @@
 declare(strict_types = 1);
 namespace Slothsoft\TalesOf\Assets;
 
-use Slothsoft\Farah\Module\Node\Asset\AssetImplementation;
+use Slothsoft\Farah\Module\Node\Asset\AssetBase;
 
-class MelnicsTest extends AssetImplementation
+class MelnicsTest extends AssetBase
 {
 }
 
