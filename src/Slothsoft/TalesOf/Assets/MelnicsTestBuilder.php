2a7558becf71ac988991b5e41675b781924fd8e8
Daniel Schulz <faulolio@gmail.com>
2018-06-23 17:50:42 +0200

    ExecutableBuilderStrategyInterface

---
+++ src/Slothsoft/TalesOf/Assets/MelnicsTestBuilder.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types = 1);
+namespace Slothsoft\TalesOf\Assets;
+
+use Slothsoft\Farah\FarahUrl\FarahUrlArguments;
+use Slothsoft\Farah\Module\Asset\AssetInterface;
+use Slothsoft\Farah\Module\Asset\ExecutableBuilderStrategy\ExecutableBuilderStrategyInterface;
+use Slothsoft\Farah\Module\Executable\ExecutableStrategies;
+
+class MelnicsTestBuilder implements ExecutableBuilderStrategyInterface
+{
+    public function buildExecutableStrategies(AssetInterface $context, FarahUrlArguments $args): ExecutableStrategies
+    {}
+
+}
+
