023dac5e317a0a40357e4dd547f80a39ad3d3947
Daniel Schulz <faulolio@gmail.com>
2024-04-01 01:25:53 +0200

    build: update files

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Slothsoft\\TalesOf\\Assets\\MelnicsTestBuilder",
+            "name": "MelnicsTestBuilder",
+            "summary": "",
+            "url": "classes/Slothsoft-TalesOf-Assets-MelnicsTestBuilder.html"
+        },                {
+            "fqsen": "\\Slothsoft\\TalesOf\\Assets\\MelnicsTestBuilder\u003A\u003AbuildExecutableStrategies\u0028\u0029",
+            "name": "buildExecutableStrategies",
+            "summary": "",
+            "url": "classes/Slothsoft-TalesOf-Assets-MelnicsTestBuilder.html#method_buildExecutableStrategies"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\Slothsoft\\TalesOf\\Assets",
+            "name": "Assets",
+            "summary": "",
+            "url": "namespaces/slothsoft-talesof-assets.html"
+        },                {
+            "fqsen": "\\Slothsoft\\TalesOf",
+            "name": "TalesOf",
+            "summary": "",
+            "url": "namespaces/slothsoft-talesof.html"
+        },                {
+            "fqsen": "\\Slothsoft",
+            "name": "Slothsoft",
+            "summary": "",
+            "url": "namespaces/slothsoft.html"
+        }            ]
+);
