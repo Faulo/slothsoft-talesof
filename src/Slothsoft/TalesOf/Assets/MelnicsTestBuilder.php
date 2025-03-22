992508acb98ee685479c150f6e05e2a95e7e6624
Daniel Schulz <faulolio@gmail.com>
2025-03-22 15:23:20 +0100

    fix: melnics maybe

--- src/Slothsoft/TalesOf/Assets/MelnicsTestBuilder.php
+++ src/Slothsoft/TalesOf/Assets/MelnicsTestBuilder.php
@@ -6,9 +6,12 @@ use Slothsoft\Farah\FarahUrl\FarahUrlArguments;
 use Slothsoft\Farah\Module\Asset\AssetInterface;
 use Slothsoft\Farah\Module\Asset\ExecutableBuilderStrategy\ExecutableBuilderStrategyInterface;
 use Slothsoft\Farah\Module\Executable\ExecutableStrategies;
+use Slothsoft\Farah\Module\Executable\ResultBuilderStrategy\NullResultBuilder;
 
 class MelnicsTestBuilder implements ExecutableBuilderStrategyInterface {
 
-    public function buildExecutableStrategies(AssetInterface $context, FarahUrlArguments $args): ExecutableStrategies {}
+    public function buildExecutableStrategies(AssetInterface $context, FarahUrlArguments $args): ExecutableStrategies {
+        return new ExecutableStrategies(new NullResultBuilder());
+    }
 }
 
